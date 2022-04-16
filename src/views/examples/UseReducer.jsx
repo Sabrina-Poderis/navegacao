ad098de27a56796b5a1f5b153845700d8df78e70
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-16 19:04:48 -0300

    Dasafio UseReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -11,7 +11,16 @@ const initialState = {
 function reducer(state, action){
     switch (action.type) {
         case 'number_add2':
-                return {...state, number: state.number + 2}
+            return {...state, number: state.number + 2}
+            break;
+        case 'number_mult7':
+            return {...state, number: state.number * 7}
+            break;
+        case 'number_divis25':
+            return {...state, number: parseInt(state.number / 25)}
+            break;
+        case 'number_addN':
+                return {...state, number: action.number}
             break;
         case 'login':
                 return {...state, user: {name: action.name}}
@@ -41,6 +50,9 @@ const UseReducer = (props) => {
                 </div>
                 <div>
                     <button className='btn' onClick={() => dispatch({type: 'number_add2'})}>+2</button>
+                    <button className='btn' onClick={() => dispatch({type: 'number_mult7'})}>*7</button>
+                    <button className='btn' onClick={() => dispatch({type: 'number_divis25'})}>/25</button>
+                    <button className='btn' onClick={() => dispatch({type: 'number_addN', number: 10})}>+ num</button>
                 </div>
             </div>
         </div>
